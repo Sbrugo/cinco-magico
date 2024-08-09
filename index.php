c982c444012b6dd8c390843bc5c6ecdc21c6711f
Sofía Brugo <brugos@jefatura.gob.ar>
2024-08-09 14:34:23 -0300

    front changes

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="./estilo.css"> 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./estilo.css"> 
     <title>Cinco Mágico</title>
 </head>
 <body>
 
-
-
-    <?php 
-    /* ACA VA EL EJECUTABLE */
+<?php 
     if(isset($_POST["juego"])){
         $txt = "";
 
@@ -20,55 +17,49 @@
         $dado3 = rand(0, 9);    
         
         $dados = [$dado1, $dado2, $dado3];
+        
         $creditos = $_POST["creditos"];
-
+        $creditos -= 10;  
+        $premios = [ 
+            0 => 0,  
+            1 => 5,   
+            2 => 25,  
+            3 => 100  
+        ];
         $numerosCinco = 0;
-
-    foreach ($dados as $dado) {
-        if ($dado === 5) {
-            $numerosCinco++;
-        }else{
-            $txt = "Ganaste 0 créditos";
+        
+        foreach ($dados as $dado) {
+            if ($dado === 5) {
+                $numerosCinco++;
+            }
         }
-    }
-    if ($numerosCinco === 1){
-        $creditos += 5;
-        $txt = "Ganaste 5 créditos";
-    }
-    if($numerosCinco === 2){
-        $creditos += 25;
-        $txt = "Ganaste 25 créditos";
-    }
-    if($numerosCinco === 3){
-        $creditos += 100;
-        $txt = "Ganaste 100 créditos";
-    }
-    }else{
+        $creditos += $premios[$numerosCinco];
+        $txt = "Ganaste " . $premios[$numerosCinco] . " créditos";
+
+    } else {
         $creditos = 100;
-        $txt = "Tira y gana créditos";
+        $txt = "Ganá o perde";
     }
-    function getDadoClass($dado) {
-        return $dado === 5 ? 'numero-cinco' : 'normal';
-    }
-    ?>
+?>
 
-    <h4>Juego de Dados</h4>
-    <div class="puntaje">
-        <p>Créditos actuales: <?php echo $creditos; ?></p>
-        <p><?php echo isset($txt) ? $txt : ""; ?></p>
+<h4>Juego de Dados</h4>
+<div class="puntaje">
+    <p>Empezás con 100 créditos</p>
+    <p><?php echo isset($txt) ? $txt : ""; ?></p>
+    <p id="creditos"><span>Créditos</span><?php echo $creditos; ?></p>
+</div>
 
-    </div>
-
- <form method="post">
-
-    <input type="hidden" name="creditos" value="<?php echo $creditos;?>">    
-    
-     <input type="text" value="<?php if(isset($dado1)) { echo $dado1; }?>">
-     <input type="text" value="<?php if(isset($dado2)) { echo $dado2; }?>">
-     <input type="text" value="<?php if(isset($dado3)) { echo $dado3; }?>">
-    
-     <button type="submit" name="juego">TIRAR</button>
-
+<?php if($creditos >= 10): ?>
+    <form method="post">
+        <input type="hidden" name="creditos" value="<?php echo $creditos; ?>">       
+        <input type="text" value="<?php if(isset($dado1)) { echo $dado1; } ?>" readonly>
+        <input type="text" value="<?php if(isset($dado2)) { echo $dado2; } ?>" readonly>
+        <input type="text" value="<?php if(isset($dado3)) { echo $dado3; } ?>" readonly>    
+        <button type="submit" name="juego">TIRAR</button>
     </form>
+<?php else: ?>
+    <p>No Podes jugar más</p>
+<?php endif; ?>
+
 </body>
 </html>
